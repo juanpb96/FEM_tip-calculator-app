2c2f4a38fa6990d1a5358750bb6d76e95d8358a1
MasterDev <juanpb.96@gmail.com>
2021-09-06 20:11:23 -0500

    Add validation for error class in inputs

--- js/utils.js
+++ js/utils.js
@@ -6,6 +6,7 @@ export const checkChanges = (target, errorElement, regEx) => {
     if (value.split('')[0] === '0') {
         errorElement.innerHTML = errorMsgZero;
         errorElement.classList.add('error--active');
+        target.classList.add('input--error');
         target.value = '';
         return;
     }
@@ -13,6 +14,7 @@ export const checkChanges = (target, errorElement, regEx) => {
     if (!regEx.test(value)) {
         errorElement.innerHTML = 'Input error';
         errorElement.classList.add('error--active');
+        target.classList.add('input--error');
         const newValue = value.split('');
         target.value = newValue.slice(0, newValue.length - 1).join('');
         return;
@@ -20,21 +22,25 @@ export const checkChanges = (target, errorElement, regEx) => {
 
     errorElement.innerHTML = '';
     errorElement.classList.remove('error--active');
+    target.classList.remove('input--error');
 }
 
 export const checkInput = (target) => {
     const { value } = target;
 
     if (value.split('')[0] === '0') {
+        target.classList.add('input--error');
         target.value = '';
         return;
     }
 
     if (!/^[0-9]{1,3}$/.test(value) || parseInt(value) > 100) {
+        target.classList.add('input--error');
         const newValue = value.split('');
         target.value = newValue.slice(0, newValue.length - 1).join('');
         return;
     }
     
+    target.classList.remove('input--error');
     return true;
 };
